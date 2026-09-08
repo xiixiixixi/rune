68c807e120ac5e4ec2645e90d4d0f64dfabbeb42
xiixiixixi <9391209+xiixiixixi@users.noreply.github.com>
2026-09-08 23:33:03 +0800

    chore: remove landing site

--- rune-landing/lib/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Space_Mono } from "next/font/google"
-
-export const spaceMono = Space_Mono({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-space-mono",
-  display: "swap",
-})
